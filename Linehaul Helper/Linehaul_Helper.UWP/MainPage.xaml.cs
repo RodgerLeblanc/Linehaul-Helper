cad7f0ce7785c4ff26edd2f76510905b54295cea
roger <roger@LAPTOP-P8ISDLO4>
2017-03-05 07:09:13 -0500

    Add project files.

---
+++ Linehaul Helper/Linehaul_Helper.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Linehaul_Helper.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Linehaul_Helper.App());
+        }
+    }
+}
